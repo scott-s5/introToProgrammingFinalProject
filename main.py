24582da5d9a68ce268694658a290ad0ea60b5a7d
scott.s <sjsmithusa@gmail.com>
2022-11-17 12:33:04 -0800

    added to main

---
+++ main.py
@@ -0,0 +1,18 @@
+''' 
+overview of game
+'''
+
+
+# sources: 
+
+#import libraries
+
+# build in 
+
+# installed modules or libraries 
+
+# created modules or libraries
+import settings
+#global variables
+
+#utility
