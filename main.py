431a2b2d86e745e6a6acf0d9fe1efdd71c6bd248
scott.s <sjsmithusa@gmail.com>
2022-11-30 09:24:16 -0800

    began setting up main with my project idea

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 ''' 
 overview of game
+
+Base game: Mouse character must catch the cheese, which moves at random speeds and directions, difficulty can be set
+- Can modify characters and objects they are chasing 
 '''
 
 
 # sources: 
 
 #import libraries
-
+import pygame as pg
+from pygame.sprite import Sprite 
+import random 
+from random import randint 
+import time
 # build in 
 
 # installed modules or libraries 
